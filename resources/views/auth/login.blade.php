57e878f9c436ec1037080d0be535614141465ae4
Mohammed Hassan <medo.hassan0100@gmail.com>
2022-09-19 11:48:51 +0200

    login footer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
     <link id="pagestyle" href="{{ asset('admin/css/material-dashboard.css?v=3.0.0') }}" rel="stylesheet" />
     <style>
         .ahmed:hover{
-            
+
         }
     </style>
 </head>
@@ -112,28 +112,16 @@
                                     document.write(new Date().getFullYear())
                                 </script>,
                                 made with <i class="fa fa-heart" aria-hidden="true"></i> by
-                                <a href="https://www.creative-tim.com" class="font-weight-bold text-white"
-                                    target="_blank">Creative Tim</a>
+                                <a href="https://sigma-business.com/" target="_blank" class="font-weight-bold text-white"
+                                    target="_blank">SIGMA</a>
                                 for a better web.
                             </div>
                         </div>
                         <div class="col-12 col-md-6">
                             <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                                 <li class="nav-item">
-                                    <a href="https://www.creative-tim.com" class="nav-link text-white"
-                                        target="_blank">Creative Tim</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="https://www.creative-tim.com/presentation" class="nav-link text-white"
+                                    <a href="https://sigma-business.com/" target="_blank" class="nav-link text-white"
                                         target="_blank">About Us</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="https://www.creative-tim.com/blog" class="nav-link text-white"
-                                        target="_blank">Blog</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-white"
-                                        target="_blank">License</a>
                                 </li>
                             </ul>
                         </div>
